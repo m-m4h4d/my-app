980bd84c8a0bfd5c8048190679276217a5d9ea64
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-07-02 14:56:27 +0500

    Added images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,9 @@ import Pd0 from './Picture0_dark.png';
 import Pd1 from './Picture1_dark.png';
 import Pl0 from './Picture0_light.png';
 import Pl1 from './Picture1_light.png';
+import PV from './Principal.jpg';
+import PS from './Principal_square.jpg';
+import AV from './Author.jpg';
+import AS from './Author_square.jpg';
 
-export { Pd0, Pd1, Pl0, Pl1 };
+export { Pd0, Pd1, Pl0, Pl1, PV, PS, AV, AS};
